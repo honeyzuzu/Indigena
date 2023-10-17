2ec84e25f4756428a6ef7edea0b9f246428caf87
Queenie Sun <queenie.sn@gmail.com>
2023-10-17 16:49:38 -0400

    Font and theme clean up, changing pages, adding search page

--- indigena/lib/main.dart
+++ indigena/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'searchPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,104 +15,12 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         textTheme: const TextTheme(
+          bodySmall: TextStyle(fontSize: 16.0,fontFamily: 'Circe'),
+          bodyLarge: TextStyle(fontSize: 20.0,fontFamily: 'Carelia'),
           // bodyText2: TextStyle(color: Colors.red, fontWeight: FontWeight.w900),
         ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  // This controller will store the value of the search bar
-  final TextEditingController _searchController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Container(//bg
-              color: Color(0xffefebe7),
-              height: MediaQuery. of(context). size. height - 16,
-              width: MediaQuery. of(context). size. width,
-
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image(image: AssetImage('assets/images/logoaltcolors.png'), height: 200,),
-
-                  Container(//green part
-                    height: 200, width: 350,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: Color(0xff406767)),
-                    
-                    
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-
-                      children: [
-                        Text("Enter your location\n to determine your local biome\n", 
-                            textAlign: TextAlign.center, 
-                            style:
-                              TextStyle(fontSize: 20, fontFamily: 'Circe', color: Color(0xffefebe7))),
-                        Container(
-                      height: 50, width: 300,
-                      alignment: Alignment.bottomCenter,
-                      decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(40),
-                      color: Color(0xffefebe7).withOpacity(0.7)),
-                      child: TextField(
-                        controller: _searchController,
-                        decoration: InputDecoration(
-                          hintText: 'Start typing...',
-                          hintStyle: TextStyle(color: Color(0xff406767)),
-                          suffixIcon: IconButton(
-                            icon: Icon(Icons.clear),
-                            color: Color(0xff406767),
-                            onPressed: () => _searchController.clear(),
-                          ),
-                          prefixIcon: IconButton(
-                            icon: Icon(Icons.search),
-                            color: Color(0xff406767),
-                            onPressed: () {
-                              // Perform the search here
-                            },
-                          ),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20.0),
-                          ),
-                        ),
-                      ),
-                    )
-                      ],
-                    )
-                  )
-                  
-                ]
-                
-          ),
-          ),
-
-          ]
-        ),
-      ),
-    );
+      home: const SearchPage(title: 'Flutter Demo Home Page'),    );
   }
 }
 
--- indigena/lib/searchPage.dart
+++ indigena/lib/searchPage.dart
@@ -1,54 +1,95 @@
-
-// Copyright 2021 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// #docregion all
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key, required this.title});
+  final String title;
 
-// #docregion MyApp
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+
+  // This controller will store the value of the search bar
+  final TextEditingController _searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter layout demo',
-      home: Scaffold(
-        // #docregion centered-text
-        body: Center(
-          child: Container(
-            width: MediaQuery. of(context). size. width,
-            color: Color(0xffefebe7),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image(image: AssetImage('assets/images/logoaltcolors.png'), height: 200,),
-                Container( 
-                  height: 200, width: 350,
-                  decoration: BoxDecoration(
-                    color: Color(0xff406767),
-                    borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(40.0),
-                        bottomRight: Radius.circular(40.0),
-                        topLeft: Radius.circular(40.0),
-                        bottomLeft: Radius.circular(40.0)),
-                  ),
-                  
-                
-              )
-              ],
 
-              
-            
-            ),
-          ),           // #enddocregion text
+    return Scaffold(
+      
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Container(//bg
+              color: Color(0xffefebe7),
+              height: MediaQuery. of(context). size. height - 16,
+              width: MediaQuery. of(context). size. width,
+
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image(image: AssetImage('assets/images/logoaltcolors.png'), height: 200,),
+
+                  Container(//green part
+                    height: 200, width: 350,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Color(0xff406767)),
+                    
+                    
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+
+                      children: [
+                        Text("Enter your location\n to determine your local biome\n", 
+                            textAlign: TextAlign.center, 
+                            style:
+                              TextStyle(fontSize: 20, fontFamily: 'Circe', color: Color(0xffefebe7))),
+                        Container(
+                      height: 50, width: 300,
+                      alignment: Alignment.bottomCenter,
+                      decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      color: Color(0xffefebe7).withOpacity(0.7)),
+                      child: TextField(
+                        controller: _searchController,
+                        decoration: InputDecoration(
+                          hintText: 'Start typing...',
+                          hintStyle: TextStyle(color: Color(0xff406767)),
+                          suffixIcon: IconButton(
+                            icon: Icon(Icons.clear),
+                            color: Color(0xff406767),
+                            onPressed: () => _searchController.clear(),
+                          ),
+                          prefixIcon: IconButton(
+                            icon: Icon(Icons.search),
+                            color: Color(0xff406767),
+                            onPressed: () {
+                              // Perform the search here
+                            },
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        ),
+                      ),
+                    )
+                      ],
+                    )
+                  )
+                  
+                ]
+                
+          ),
+          ),
+
+          ]
         ),
-        // #enddocregion centered-text
       ),
     );
   }
 }
-// #enddocregion all
+
